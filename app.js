02439bd091f1a45290a5d7951760cced559c4639
Rahman <rahmanromli@yahoo.com>
2017-09-16 18:16:01 +0800

    Add react app in app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+class App extends React.Component {
+	render () {
+		return <h1>Hello world</h1>
+	}
+}
+
+ReactDom.render(<App />,
+	document.getElementById('app'))
